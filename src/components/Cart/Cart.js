39a4a29d4212c1e6f36e6c9762c7e8f7786a1170
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-01 14:02:32 +0600

    add Conditional Rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Cart = ({cart,handleRemoveFromCart}) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+
+    let command
+    if (cart.length === 0) {
+        command = <p>Please at list add one item...!</p>
+    }else if(cart.length === 1) {
+        command = <p>If you want, add one more item...!</p>
+    }else {
+        command = <p>Thanks for adding...!</p>
+    }
+
     return (
         <div>
             <h2>Selected Items: {cart.length}</h2>
+            {command}
             {
                 cart.map(tshirt => <p key={tshirt._id}>{tshirt.name} | {tshirt.price}$ | <button onClick={() => handleRemoveFromCart(tshirt)}>X</button></p>)
             }
+            {cart.length === 3 && <button>Review</button>}
         </div>
     );
 };
